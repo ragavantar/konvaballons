646d9c3fec3b8d0e7c1362ead2f019d6b691b623
Hariraagavanthar C R <ragavantar@gmail.com>
2019-07-09 12:41:42 +0530

    Updates

---
+++ precache-manifest.c170b886e5fa0f35c147cdcf6b72df44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca23373e2a21fb247cd53e61391dfef",
+    "url": "/konvaballons/index.html"
+  },
+  {
+    "revision": "e36ee18efa2c808b1d9e",
+    "url": "/konvaballons/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "98f8a5f050f6127d983f",
+    "url": "/konvaballons/static/js/2.dd128db0.chunk.js"
+  },
+  {
+    "revision": "e36ee18efa2c808b1d9e",
+    "url": "/konvaballons/static/js/main.d2f04fe9.chunk.js"
+  },
+  {
+    "revision": "7f214fa32eff04b19582",
+    "url": "/konvaballons/static/js/runtime~main.69d7f821.js"
+  }
+]);
